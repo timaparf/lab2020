b57ebfa5959525402d0068e69c9d70d64f75cb0c
Timothy Parfeniuk <timaparf@gmail.com>
2020-09-11 09:22:50 +0300

    First modification

---
+++ Lab 1.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Lab_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+
+                SmtpClient C = new SmtpClient("mail.univ.net.ua");
+                C.Port = 25;
+                C.Credentials = new NetworkCredential("CE2020test", "1q2w3e4r5t");
+                C.EnableSsl = true;
+                string text = DateTime.Now.ToString() + "\nTymofii Parfeniuk";
+                C.Send(new MailMessage("CE2020test@univ.net.ua", args[0], args[1], text));
+            }
+            else
+            {
+                Console.WriteLine("SYNTAX: mail-1 <ToAddr> <Theme>");
+            }
+        }
+    }
+}
